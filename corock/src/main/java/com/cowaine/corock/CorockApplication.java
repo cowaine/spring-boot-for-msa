e26a2c5806c2cc2024afa5e53c604a3ff74fa85d
cororing <corock.dev@gmail.com>
2023-08-07 22:59:15 +0900

    feat: 스프링 애플리케이션 ConfigurableApplicationContext 인터페이스 사용 예제 추가

--- corock/src/main/java/com/cowaine/corock/CorockApplication.java
+++ corock/src/main/java/com/cowaine/corock/CorockApplication.java
@@ -1,13 +1,26 @@
 package com.cowaine.corock;
 
+import java.util.stream.IntStream;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+@Slf4j
 @SpringBootApplication
 public class CorockApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CorockApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(CorockApplication.class, args);
+
+        Environment env = ctx.getBean(Environment.class);
+        String port = env.getProperty("server.port");
+        log.info("Customized Port: {}", port);
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        IntStream.range(0, beanNames.length)
+                 .forEach(i -> log.info("Bean Name #{}: {}", i + 1, beanNames[i]));
     }
 
 }
