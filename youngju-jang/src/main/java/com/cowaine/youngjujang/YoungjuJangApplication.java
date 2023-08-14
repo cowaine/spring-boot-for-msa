0f847e732281d5f3c4566ad5c69977615650361f
Youngju-Jang <joj1043@nate.com>
2023-08-14 21:18:38 +0900

    [Feat] 3.3 스테레오 타입 스프링 빈 사용(119p)

--- youngju-jang/src/main/java/com/cowaine/youngjujang/YoungjuJangApplication.java
+++ youngju-jang/src/main/java/com/cowaine/youngjujang/YoungjuJangApplication.java
@@ -1,5 +1,6 @@
 package com.cowaine.youngjujang;
 
+import com.cowaine.youngjujang.ch2.domain.Formatter;
 import com.cowaine.youngjujang.ch2.domain.PriceUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -8,29 +9,22 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Locale;
+
 
 @Slf4j
 @SpringBootApplication
 public class YoungjuJangApplication {
     public static void main(String[] args) {
+        
         ConfigurableApplicationContext ctx = SpringApplication.run(YoungjuJangApplication.class, args);
-          
-          /*
-		Environment env = ctx.getBean(Environment.class);
-		String portValue = env.getProperty("server.port");
-		log.info("Customiazed Port : {}", portValue);
-		
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.stream(beanNames).forEach(name -> log.info("Bean Name: {}", name));
-		 */
         
-        PriceUnit defaultPriceUnit = ctx.getBean(("priceUnit"), PriceUnit.class);
-        log.info("Price #1 : {}", defaultPriceUnit.format(BigDecimal.valueOf(10.2)));
+        // 클래스타입도 같이 보냄 >> 클래스타입의 객체로 리턴해줌 (클래스타입으로의 casting이 필요없음)
+        Formatter formatter = ctx.getBean("localDateTimeFormatter", Formatter.class);
+        String date = formatter.of(LocalDateTime.of(2020, 12, 24, 23, 59, 59));
         
-        PriceUnit wonPriceUnit = ctx.getBean(("wonPriceUnit"), PriceUnit.class);
-        log.info("Price #2 : {}", wonPriceUnit.format(BigDecimal.valueOf(1000)));
-        
+        log.info("Date : " + date);
         ctx.close();
     }
     
