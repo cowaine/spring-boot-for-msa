3922e8f707d18d63c32fbec92f0c8a7413bd24e1
bunsung92 <bunsung92@naver.com>
2023-08-14 14:41:45 +0900

    refactor: 실제 코드 추가 `@Lazy`

--- ding-cook/src/main/java/com/cowaine/dingcook/domain/ex09/SpringBean09Application.java
+++ ding-cook/src/main/java/com/cowaine/dingcook/domain/ex09/SpringBean09Application.java
@@ -1,2 +1,32 @@
-package com.cowaine.dingcook.domain.ex09;public class SpringBean09Application {
+package com.cowaine.dingcook.domain.ex09;
+
+import com.cowaine.dingcook.domain.ex01.PriceUnit;
+import java.util.Locale;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+
+@Slf4j
+@SpringBootApplication
+public class SpringBean09Application {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBean09Application.class);
+        log.info("----- Done to initialize Spring Beans -----");
+
+        PriceUnit priceUnit = applicationContext.getBean("lazyPriceUnit", PriceUnit.class);
+        log.info("Locale in PriceUnit : {}", priceUnit.getLocale().toString());
+
+        applicationContext.close();
+    }
+
+    @Bean
+    @Lazy
+    public PriceUnit lazyPriceUnit() {
+        log.info("initialize lazyPriceUnit");
+        return new PriceUnit(Locale.US);
+    }
 }
