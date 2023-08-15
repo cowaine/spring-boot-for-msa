90d7a9e0e8c78be391ac69d00bf6357bdec5a7dc
Youngju-Jang <joj1043@nate.com>
2023-08-15 23:41:38 +0900

    [Feat] 3.4 의존성 & 3.5 ApplicationContext & 3.6 Bean Scope
    ## 3.4 의존성
    - A .class(상위클래스) -> B.class(하위클래스)
    - A클래스에서 B메소드 사용, B메소드 없이 동작할 수 없음 == A클래스는 B클래스에 의존한다
    - A의 메소드내부에서 B객체를 직접 생성 > ₩강한 결합₩
    - 쉽게 변하는것 보다, 변화가 없는 것에 의존하라
    
    ## 3.6 Bean Scope
    
    - 150p 스레드 안전하지않은 코드 + singleton 스코프 예제
    
    related to #12

--- youngju-jang/src/main/java/com/cowaine/youngjujang/YoungjuJangApplication.java
+++ youngju-jang/src/main/java/com/cowaine/youngjujang/YoungjuJangApplication.java
@@ -1,39 +1,59 @@
 package com.cowaine.youngjujang;
 
-import com.cowaine.youngjujang.ch2.domain.Formatter;
+import com.cowaine.youngjujang.ch2.domain.DateFormatter;
 import com.cowaine.youngjujang.ch2.domain.PriceUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 
 @Slf4j
 @SpringBootApplication
 public class YoungjuJangApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         
         ConfigurableApplicationContext ctx = SpringApplication.run(YoungjuJangApplication.class, args);
         
-        // 클래스타입도 같이 보냄 >> 클래스타입의 객체로 리턴해줌 (클래스타입으로의 casting이 필요없음)
-        Formatter formatter = ctx.getBean("localDateTimeFormatter", Formatter.class);
-        String date = formatter.of(LocalDateTime.of(2020, 12, 24, 23, 59, 59));
+        ThreadPoolTaskExecutor taskExecutor = ctx.getBean(ThreadPoolTaskExecutor.class);
         
-        log.info("Date : " + date);
+//        final String dateString = "2020-12-24T23:59:59.-08:00";
+        final String dateString = "2020-12-24T23:59:59";
+        
+        for(int i=0; i<100; i++){
+            // submit 100회 실행
+            taskExecutor.submit(()->{
+                try{
+                    DateFormatter formatter = ctx.getBean("singletonDateFormatter", DateFormatter.class);
+                    // 스레드에 안전하다면? 출력되는 값이 모두 동일한 날짜일 것
+                    log.info("Date : {}, hashCode : {}", formatter.parse(dateString), formatter.hashCode());
+                }catch (Exception e){
+                    log.error("error to parse", e);
+                }
+            });
+        }
+        TimeUnit.SECONDS.sleep(5);
         ctx.close();
     }
     
+    @Bean // default Scope : singleton >> 여러곳에 의존성주입되어도 하나의 스프링빈이 된다.
+    public DateFormatter singletonDateFormatter(){
+        return new DateFormatter("yyyy-MM-dd'T'HH:mm:ss");
+    }
+    
     @Bean (name="priceUnit")
+    @Scope("singleton")
     public PriceUnit dollarPriceUnit(){
         return new PriceUnit(Locale.US);
     }
     
     @Bean
+    @Scope("prototype")
     public PriceUnit wonPriceUnit(){
         return new PriceUnit(Locale.KOREA);
     }
