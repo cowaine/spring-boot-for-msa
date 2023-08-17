06897a4f4a5fe11ec881c2e6940403e8080f70cc
Youngju-Jang <joj1043@nate.com>
2023-08-17 23:42:43 +0900

    [Feat] 3.7 스프링 빈 생명주기 관리 (152p ~ 157p)

--- youngju-jang/src/main/java/com/cowaine/youngjujang/YoungjuJangApplication.java
+++ youngju-jang/src/main/java/com/cowaine/youngjujang/YoungjuJangApplication.java
@@ -1,7 +1,11 @@
 package com.cowaine.youngjujang;
 
 import com.cowaine.youngjujang.ch2_3.domain.DateFormatter;
+import com.cowaine.youngjujang.ch2_3.domain.LifeCycleComponent;
+import com.cowaine.youngjujang.ch2_3.domain.LifeCycleComponentVer2;
+import com.cowaine.youngjujang.ch2_3.domain.PrintableBeanPostProcessor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -18,31 +22,20 @@ public class YoungjuJangApplication {
     public static void main(String[] args) throws InterruptedException {
         
         ConfigurableApplicationContext ctx = SpringApplication.run(YoungjuJangApplication.class, args);
-        
-        ThreadPoolTaskExecutor taskExecutor = ctx.getBean(ThreadPoolTaskExecutor.class);
-        
-        final String dateString = "2020-12-24T23:59:59.-08:00";
-//        final String dateString = "2020-12-24T23:59:59";
-        
-        for(int i=0; i<100; i++){
-            // submit 100회 실행
-            taskExecutor.submit(()->{
-                try{
-                    DateFormatter formatter = ctx.getBean("singletonDateFormatter", DateFormatter.class);
-                    // 스레드에 안전하다면? 출력되는 값이 모두 동일한 날짜일 것
-                    log.info("Date : {}, hashCode : {}", formatter.parse(dateString), formatter.hashCode());
-                }catch (Exception e){
-                    log.error("error to parse", e);
-                }
-            });
-        }
-        TimeUnit.SECONDS.sleep(5);
         ctx.close();
+    }
+    @Bean(initMethod = "init", destroyMethod = "clear")
+    public LifeCycleComponent lifecycleComponent(){
+        return new LifeCycleComponent();
+    }
+    
+    @Bean(initMethod = "init", destroyMethod = "clear")
+    public LifeCycleComponentVer2 lifeCycleComponentVer2(){
+        return new LifeCycleComponentVer2();
     }
     
     @Bean
-    @Scope("prototype")
-    public DateFormatter singletonDateFormatter(){
-        return new DateFormatter("yyyy-MM-dd'T'HH:mm:ss");
+    public BeanPostProcessor beanPostProcessor(){
+        return new PrintableBeanPostProcessor();
     }
 }
