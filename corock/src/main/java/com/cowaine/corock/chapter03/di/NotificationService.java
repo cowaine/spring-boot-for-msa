a6495564f9effb603a5a7099d64b8c8a7e773dcb
cororing <corock.dev@gmail.com>
2023-08-19 21:25:36 +0900

    fix: 의존 관계를 맺을 때 쉽게 변하는 것보다 변화가 없는 것에 의존하라

--- corock/src/main/java/com/cowaine/corock/chapter03/di/NotificationService.java
+++ corock/src/main/java/com/cowaine/corock/chapter03/di/NotificationService.java
@@ -5,13 +5,15 @@ import com.cowaine.corock.chapter03.User;
 public class NotificationService {
 
     public boolean sendNotification(User user, String message) {
-        SmsSender smsSender = SmsSender.builder()
+        // SmsSender smsSender = SmsSender.builder()
+        AppPushSender appPushSender = AppPushSender.builder()
                 .endpoint("/send")
                 .port(9180)
                 .timeout(4_000L)
                 .build();
 
-        return smsSender.sendText(user.getPhoneNumber(), message);
+        // return smsSender.sendText(user.getPhoneNumber(), message);
+        return appPushSender.sendText(user.getPhoneNumber(), message);
     }
 
 }
