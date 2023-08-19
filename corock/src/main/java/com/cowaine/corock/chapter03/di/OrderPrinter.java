cbeb7f6c3ecb4eb65379fedc1f3e9b8578ab0b73
cororing <corock.dev@gmail.com>
2023-08-19 22:08:34 +0900

    feat: 애너테이션 기반 설정의 의존성 주입 예제

---
+++ corock/src/main/java/com/cowaine/corock/chapter03/di/OrderPrinter.java
@@ -0,0 +1,41 @@
+package com.cowaine.corock.chapter03.di;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.time.LocalDateTime;
+
+@Service
+public class OrderPrinter implements Printer<OrderProduct> {
+
+    // 1. Field Injection (Field injection is not recommended)
+    @Autowired
+    @Qualifier("localDateTimeFormatter")
+    private Formatter<LocalDateTime> formatter01;
+
+    private Formatter<LocalDateTime> formatter02;
+
+    // 2. Setter Method Injection
+    @Autowired
+    public void setFormatter02(@Qualifier("localDateTimeFormatter") Formatter<LocalDateTime> formatter) {
+        this.formatter02 = formatter;
+    }
+
+    private Formatter<LocalDateTime> formatter03;
+
+    // 3. Constructor Injection
+    @Autowired
+    public OrderPrinter(@Qualifier("localDateTimeFormatter") Formatter<LocalDateTime> formatter03) {
+        this.formatter03 = formatter03;
+    }
+
+    @Override
+    public void print(OutputStream os, OrderProduct orderProduct) throws IOException {
+        String orderAt = formatter03.of(orderProduct.getOrderAt());
+        os.write(orderAt.getBytes());
+    }
+
+}
