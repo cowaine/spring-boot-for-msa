72816487e02024a67d947f8b1f624a37eaf6d183
cororing <corock.dev@gmail.com>
2023-08-19 21:19:20 +0900

    feat: SMS 발송 예제 추가

---
+++ corock/src/main/java/com/cowaine/corock/chapter03/di/NotificationService.java
@@ -0,0 +1,17 @@
+package com.cowaine.corock.chapter03.di;
+
+import com.cowaine.corock.chapter03.User;
+
+public class NotificationService {
+
+    public boolean sendNotification(User user, String message) {
+        SmsSender smsSender = SmsSender.builder()
+                .endpoint("/send")
+                .port(9180)
+                .timeout(4_000L)
+                .build();
+
+        return smsSender.sendText(user.getPhoneNumber(), message);
+    }
+
+}
---
+++ corock/src/main/java/com/cowaine/corock/chapter03/di/SmsSender.java
@@ -0,0 +1,18 @@
+package com.cowaine.corock.chapter03.di;
+
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Builder
+public class SmsSender {
+
+    private final String endpoint;
+    private final int port;
+    private final long timeout;
+
+    public boolean sendText(String phoneNumber, String message) {
+        return false;
+    }
+
+}
