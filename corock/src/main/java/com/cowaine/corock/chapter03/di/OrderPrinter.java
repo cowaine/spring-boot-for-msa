aa04289c8c871529a9787886adea5235cf19fa1a
cororing <corock.dev@gmail.com>
2023-08-19 22:10:57 +0900

    refactor: Autowired 가 생략된 생성자 주입 예제

--- corock/src/main/java/com/cowaine/corock/chapter03/di/OrderPrinter.java
+++ corock/src/main/java/com/cowaine/corock/chapter03/di/OrderPrinter.java
@@ -1,6 +1,5 @@
 package com.cowaine.corock.chapter03.di;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -12,29 +11,28 @@ import java.time.LocalDateTime;
 public class OrderPrinter implements Printer<OrderProduct> {
 
     // 1. Field Injection (Field injection is not recommended)
-    @Autowired
-    @Qualifier("localDateTimeFormatter")
-    private Formatter<LocalDateTime> formatter01;
+    // @Autowired
+    // @Qualifier("localDateTimeFormatter")
+    // private Formatter<LocalDateTime> formatter01;
 
-    private Formatter<LocalDateTime> formatter02;
+    // private Formatter<LocalDateTime> formatter02;
 
     // 2. Setter Method Injection
-    @Autowired
-    public void setFormatter02(@Qualifier("localDateTimeFormatter") Formatter<LocalDateTime> formatter) {
-        this.formatter02 = formatter;
-    }
+    // @Autowired
+    // public void setFormatter02(@Qualifier("localDateTimeFormatter") Formatter<LocalDateTime> formatter) {
+    //     this.formatter02 = formatter;
+    // }
 
-    private Formatter<LocalDateTime> formatter03;
+    private Formatter<LocalDateTime> formatter;
 
     // 3. Constructor Injection
-    @Autowired
-    public OrderPrinter(@Qualifier("localDateTimeFormatter") Formatter<LocalDateTime> formatter03) {
-        this.formatter03 = formatter03;
+    public OrderPrinter(@Qualifier("localDateTimeFormatter") Formatter<LocalDateTime> formatter) {
+        this.formatter = formatter;
     }
 
     @Override
     public void print(OutputStream os, OrderProduct orderProduct) throws IOException {
-        String orderAt = formatter03.of(orderProduct.getOrderAt());
+        String orderAt = formatter.of(orderProduct.getOrderAt());
         os.write(orderAt.getBytes());
     }
 
