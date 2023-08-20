c8db8fa4e822429f756ed55bdb310737b683b083
JoisFe <rhsnqk@gmail.com>
2023-08-20 16:15:41 +0900

    03. 스프링 애플리케이션 기본 - @Bean 애너테이션 사용

--- joisfe/src/main/java/com/cowaine/joisfe/JoisfeApplication.java
+++ joisfe/src/main/java/com/cowaine/joisfe/JoisfeApplication.java
@@ -1,13 +1,38 @@
 package com.cowaine.joisfe;
 
+import com.cowaine.joisfe.part3.PriceUnit;
+import java.math.BigDecimal;
+import java.util.Locale;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
+@Slf4j
 @SpringBootApplication
 public class JoisfeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JoisfeApplication.class, args);
+        ConfigurableApplicationContext ctxt = SpringApplication.run(JoisfeApplication.class, args);
+
+        PriceUnit defaultPriceUnit = ctxt.getBean("priceUnit", PriceUnit.class);
+        log.info("Price #1 : {}", defaultPriceUnit.format(BigDecimal.valueOf(10.2)));
+
+        PriceUnit wonPriceUnit = ctxt.getBean("wonPriceUnit", PriceUnit.class);
+        log.info("Price #2 : {}", wonPriceUnit.format(BigDecimal.valueOf(1000)));
+
+        ctxt.close();
     }
 
+    @Bean(name = "priceUnit")
+    public PriceUnit dolloarPriceUnit() {
+        return new PriceUnit(Locale.US);
+    }
+
+    @Bean
+    public PriceUnit wonPriceUnit() {
+        return new PriceUnit(Locale.KOREA);
+    }
 }
