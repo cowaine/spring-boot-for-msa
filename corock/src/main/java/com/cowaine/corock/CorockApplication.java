69749b56df35509d4a37f05173ffcc07bd2eaa0d
cororing <corock.dev@gmail.com>
2023-08-22 14:26:55 +0900

    feat: 스프링 빈 스코프

--- corock/src/main/java/com/cowaine/corock/CorockApplication.java
+++ corock/src/main/java/com/cowaine/corock/CorockApplication.java
@@ -1,33 +1,51 @@
 package com.cowaine.corock;
 
-import com.cowaine.corock.chapter03.di.Formatter;
 import com.cowaine.corock.chapter03.PriceUnit;
+import com.cowaine.corock.chapter03.di.DateFormatter;
+import com.cowaine.corock.chapter03.di.Formatter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.math.BigDecimal;
+import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 @Slf4j
 @SpringBootApplication
 public class CorockApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ConfigurableApplicationContext ctx = SpringApplication.run(CorockApplication.class, args);
 
         // CorockApplication.p99(ctx);
         // CorockApplication.p110(ctx);
+        // CorockApplication.p121(ctx);
 
-        Formatter<LocalDateTime> formatter = ctx.getBean("localDateTimeFormatter", Formatter.class);
-        String date = formatter.of(LocalDateTime.of(2020, 12, 24, 23, 59, 59));
+        ThreadPoolTaskExecutor taskExecutor = ctx.getBean(ThreadPoolTaskExecutor.class);
+        final String dateString = "2020-12-24T23:59:59.-08:00";
 
-        log.info("Date: " + date);
+        for (int i = 0; i < 100; i++) {
+            taskExecutor.submit(() -> {
+                DateFormatter dateFormatter = ctx.getBean("singletonDateFormatter", DateFormatter.class);
+                try {
+                    log.info("Date: {}, hashCode: {}", dateFormatter.parse(dateString), dateFormatter.hashCode());
+                } catch (ParseException e) {
+                    log.error("error to parse", e);
+                }
+            });
+        }
+
+        TimeUnit.SECONDS.sleep(5);
+
         ctx.close();
     }
 
@@ -60,6 +78,19 @@ public class CorockApplication {
     @Bean
     public PriceUnit wonPriceUnit() {
         return new PriceUnit(Locale.KOREA);
+    }
+
+    private static void p121(ConfigurableApplicationContext ctx) {
+        Formatter<LocalDateTime> formatter = ctx.getBean("localDateTimeFormatter", Formatter.class);
+        String date = formatter.of(LocalDateTime.of(2020, 12, 24, 23, 59, 59));
+
+        log.info("Date: " + date);
+    }
+
+    @Scope(scopeName = "prototype")
+    @Bean
+    public DateFormatter singletonDateFormatter() {
+        return new DateFormatter("yyyy-MM-dd'T'HH:mm:ss");
     }
 
 }
--- corock/src/main/java/com/cowaine/corock/chapter03/di/DateFormatter.java
+++ corock/src/main/java/com/cowaine/corock/chapter03/di/DateFormatter.java
@@ -1,5 +1,8 @@
 package com.cowaine.corock.chapter03.di;
 
+import org.springframework.util.StringUtils;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -8,12 +11,19 @@ public class DateFormatter implements Formatter<Date> {
     private SimpleDateFormat simpleDateFormat;
 
     public DateFormatter(String pattern) {
+        if (/* StringUtils.isEmpty(pattern) */ !StringUtils.hasLength(pattern)) {
+            throw new IllegalArgumentException("Pattern is empty");
+        }
         this.simpleDateFormat = new SimpleDateFormat(pattern);
     }
 
     @Override
     public String of(Date target) {
         return simpleDateFormat.format(target);
+    }
+
+    public Date parse(String dateString) throws ParseException {
+        return simpleDateFormat.parse(dateString);
     }
 
 }
