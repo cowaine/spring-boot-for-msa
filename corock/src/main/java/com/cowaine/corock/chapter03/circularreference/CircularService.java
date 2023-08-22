d3d96da28a585246359a366a297dab8d29516393
cororing <corock.dev@gmail.com>
2023-08-22 13:58:05 +0900

    feat: 순환 참조 시 BeanCurrentlyInCreationException 발생?

---
+++ corock/src/main/java/com/cowaine/corock/chapter03/circularreference/CircularService.java
@@ -0,0 +1,14 @@
+package com.cowaine.corock.chapter03.circularreference;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CircularService {
+
+    private CircularReference circularReference;
+
+    public CircularService(CircularReference circularReference) {
+        this.circularReference = circularReference;
+    }
+
+}
