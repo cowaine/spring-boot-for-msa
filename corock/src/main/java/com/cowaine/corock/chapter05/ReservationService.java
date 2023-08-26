8e7026af896233942e8d337575eec3e2c1baab8a
cororing <corock.dev@gmail.com>
2023-08-26 15:50:40 +0900

    refactor: ControllerAdvice 과 ExceptionHandler 애너테이션 예외 처리

--- corock/src/main/java/com/cowaine/corock/chapter05/ReservationService.java
+++ corock/src/main/java/com/cowaine/corock/chapter05/ReservationService.java
@@ -8,6 +8,12 @@ import java.time.LocalDate;
 public class ReservationService {
 
     public Long reserveHotelRoom(Long hotelId, String roomNumber, LocalDate checkInDate, LocalDate checkOutDate) {
+        // hotelRoomRepository.findByHotelIdAndRoomNumber(hotelId, roomNumber)
+        //         .orElseThrow(() -> {
+        //             log.error("Invalid roomNumber. hotelId: {}, roomNumber: {}", hotelId, roomNumber);
+        //             return new BadRequestException("Not existing roomNumber");
+        //         });
+
         return 1_002_003_004L;
     }
 
