ddd76260c11c145f2affc8ee7ba9c19a923acd4d
cororing <corock.dev@gmail.com>
2023-08-26 13:44:45 +0900

    feat: REST-API 응답 메시지 처리

--- corock/src/main/java/com/cowaine/corock/chapter05/HotelRoomResponse.java
+++ corock/src/main/java/com/cowaine/corock/chapter05/HotelRoomResponse.java
@@ -1,6 +1,11 @@
 package com.cowaine.corock.chapter05;
 
 import com.cowaine.corock.chapter05.util.IdGenerator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,13 +14,21 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class HotelRoomResponse {
 
+    @JsonProperty(namespace = "id")
+    @JsonSerialize(using = ToStringSerializer.class)
     private final Long hotelRoomId;
+
     private final String roomNumber;
+
     private final HotelRoomType hotelRoomType;
+
+    @JsonSerialize(using = ToDollarStringSerializer.class)
     private final BigDecimal originalPrice;
+
     private final List<Reservation> reservations;
 
     public static HotelRoomResponse of(Long hotelRoomId, String roomNumber, HotelRoomType hotelRoomType, BigDecimal originalPrice) {
@@ -29,8 +42,14 @@ public class HotelRoomResponse {
     @RequiredArgsConstructor
     @Getter
     private static class Reservation {
+
+        @JsonProperty(namespace = "id")
+        @JsonSerialize(using = ToStringSerializer.class)
         private final Long reservationId;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         private final LocalDate reservedDate;
+
     }
 
 }
---
+++ corock/src/main/java/com/cowaine/corock/chapter05/ToDollarStringSerializer.java
@@ -0,0 +1,18 @@
+package com.cowaine.corock.chapter05;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class ToDollarStringSerializer extends JsonSerializer<BigDecimal> {
+
+    @Override
+    public void serialize(BigDecimal value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeString(value.setScale(2, RoundingMode.UNNECESSARY).toString());
+    }
+
+}
