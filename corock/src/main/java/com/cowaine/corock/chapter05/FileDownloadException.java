14772395174023966dbc9a622fdb1c9979518f6b
cororing <corock.dev@gmail.com>
2023-08-26 16:07:36 +0900

    feat: 미디어 콘텐츠 내려받기

---
+++ corock/src/main/java/com/cowaine/corock/chapter05/FileDownloadException.java
@@ -0,0 +1,9 @@
+package com.cowaine.corock.chapter05;
+
+public class FileDownloadException extends RuntimeException {
+
+    public FileDownloadException(String message) {
+        super(message);
+    }
+
+}
