e77b8331dd5287374258ebf86dd2ff8da62c676c
Youngju-Jang <joj1043@nate.com>
2023-08-28 23:46:30 +0900

    [Feat] ~5.1.6 @RequestHeader, @DeleteMapping

--- youngju-jang/src/main/java/com/cowaine/youngjujang/ch5/domain/controller/HotelRoomController.java
+++ youngju-jang/src/main/java/com/cowaine/youngjujang/ch5/domain/controller/HotelRoomController.java
@@ -1,12 +1,10 @@
 package com.cowaine.youngjujang.ch5.domain.controller;
 
+import com.cowaine.youngjujang.ch5.domain.dto.DeleteResultResponse;
 import com.cowaine.youngjujang.ch5.domain.dto.HotelRoomResponse;
 import com.cowaine.youngjujang.ch5.domain.dto.HotelRoomType;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -23,7 +21,11 @@ public class HotelRoomController {
           @RequestParam (value = "fromDate", required = false)
           @DateTimeFormat (pattern = "yyyyMMdd") LocalDate fromDate,
           @RequestParam (value = "toDate", required = false)
-          @DateTimeFormat (pattern = "yyyyMMdd") LocalDate toDate
+          @DateTimeFormat (pattern = "yyyyMMdd") LocalDate toDate // 파라미터값 문자열을 LocalDate 타입으로 변환할때 패턴으로 파싱하여 변환. PathVariable 등에도 사용가능
+//          @RequestHeader(value = "x-from-date", required = false)
+//          @DateTimeFormat (pattern = "yyyyMMdd") LocalDate fromDate,
+//          @RequestHeader(value = "x-to-date", required = false)
+//          @DateTimeFormat (pattern = "yyyyMMdd") LocalDate toDate,
      ) {
           Long hotelRoomId = new Date().getTime();
           BigDecimal originalPrice = new BigDecimal("130.00");
@@ -36,8 +38,16 @@ public class HotelRoomController {
           );
           if (Objects.nonNull(fromDate) && Objects.nonNull(toDate)) {
                fromDate.datesUntil(toDate.plusDays(1))
-                    .forEach(date -> response.reservedAt(date));
+                    .forEach(response::reservedAt);
           }
           return response;
+     }
+     
+     @DeleteMapping(path = "/hotels/{hotelId}/rooms/{roomNumber}")
+     public DeleteResultResponse deleteHotelRoome(
+          @PathVariable Long hotelId,
+          @PathVariable String roomNumber){
+          System.out.println("Delete Request. hotelId = " + hotelId);
+          return new DeleteResultResponse(Boolean.TRUE, "success");
      }
 }
---
+++ youngju-jang/src/main/java/com/cowaine/youngjujang/ch5/domain/dto/DeleteResultResponse.java
@@ -0,0 +1,10 @@
+package com.cowaine.youngjujang.ch5.domain.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class DeleteResultResponse {
+     private Boolean isSuccess;
+     private String result;
+}
+
