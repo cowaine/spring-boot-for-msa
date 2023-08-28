99056abf8d1d378b66e3230231de9e67ba903fe9
crupy <wlxh95@naver.com>
2023-08-28 16:49:40 +0900

    [Feat] part5 예제 코드 완료
    part5에 있는 예제 코드 작성 완료

---
+++ crupy/src/main/java/com/cowaine/crupy/part5/controller/ApiExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.cowaine.crupy.part5.controller;
+
+import com.cowaine.crupy.part5.exception.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex){
+        System.out.println("Error Message : " + ex.getErrorMessage());
+        return new ResponseEntity<>(
+                new ErrorResponse(ex.getErrorMessage()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex){
+        return new ResponseEntity<>(
+                new ErrorResponse("system error"),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+}
