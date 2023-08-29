f9d29bbe4e13f1117f43684e8f963d24449f82e9
JoisFe <rhsnqk@gmail.com>
2023-08-29 21:31:11 +0900

    05. 스프링 MVC를 이용한 REST_API 개발
    - REST-API GET, DELETE, POST, PUT 메서드 매핑
    - 응답 처리 방법
    - ResponseEntity 객체를 이용한 응답
    - Pageable, Sort
    - 검증, 예외처리
    - 미디어 컨텐츠 받기

---
+++ joisfe/src/main/java/com/cowaine/joisfe/part5/dto/DeleteResultResponse.java
@@ -0,0 +1,10 @@
+package com.cowaine.joisfe.part5.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class DeleteResultResponse {
+
+    private boolean success;
+    private String message;
+}
---
+++ joisfe/src/main/java/com/cowaine/joisfe/part5/dto/HotelRoomIdResponse.java
@@ -0,0 +1,24 @@
+package com.cowaine.joisfe.part5.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import java.util.Objects;
+
+public class HotelRoomIdResponse {
+
+    @JsonProperty("id")
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long hotelRoomId;
+
+    private HotelRoomIdResponse(Long hotelRoomId) {
+        if (Objects.isNull(hotelRoomId))
+            throw new IllegalArgumentException("hotelRoomId is null");
+
+        this.hotelRoomId = hotelRoomId;
+    }
+
+    public static HotelRoomIdResponse from(Long hotelRoomId) {
+        return new HotelRoomIdResponse(hotelRoomId);
+    }
+}
