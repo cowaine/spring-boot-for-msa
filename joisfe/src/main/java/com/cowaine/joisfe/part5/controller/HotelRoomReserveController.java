c1abc7e56aa54830e75bb7d343b3b2d5c7b83b3d
JoisFe <rhsnqk@gmail.com>
2023-09-04 23:41:59 +0900

    05. 스프링 MVC를 이용한 REST_API 개발
    - 빠트린 @RestController 추가

--- joisfe/src/main/java/com/cowaine/joisfe/part5/controller/HotelRoomReserveController.java
+++ joisfe/src/main/java/com/cowaine/joisfe/part5/controller/HotelRoomReserveController.java
@@ -14,8 +14,10 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RestController
 public class HotelRoomReserveController {
 
     private final ReserveService reserveService;
