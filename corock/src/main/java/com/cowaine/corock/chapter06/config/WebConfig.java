91fed230836d2e27caaadb37b747c89ce9dc91ee
cororing <corock.dev@gmail.com>
2023-09-17 18:58:58 +0900

    feat: HttpMessageConverter 와 REST-API 설정

--- corock/src/main/java/com/cowaine/corock/chapter06/config/WebConfig.java
+++ corock/src/main/java/com/cowaine/corock/chapter06/config/WebConfig.java
@@ -1,16 +1,38 @@
 package com.cowaine.corock.chapter06.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean(name = "localeResolver")
     public LocaleResolver localeResolver() {
         return new AcceptHeaderLocaleResolver();
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter());
+        // converters.add(new MappingJackson2XmlHttpMessageConverter());
+    }
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+        return objectMapper;
     }
 
 }
