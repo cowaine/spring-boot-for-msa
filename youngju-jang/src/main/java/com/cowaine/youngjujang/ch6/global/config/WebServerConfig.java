e97e755566995028a5d14cae98a393721ec23937
Youngju-Jang <joj1043@nate.com>
2023-10-05 19:47:00 +0900

    [Feat] 6.2 HttpMessageConverter 설정 (312p~)
    - accept header 에 따른 코드변경 없이 사용할 수 있도록 설정 추가 가능

--- youngju-jang/src/main/java/com/cowaine/youngjujang/ch6/global/config/WebServerConfig.java
+++ youngju-jang/src/main/java/com/cowaine/youngjujang/ch6/global/config/WebServerConfig.java
@@ -4,6 +4,9 @@ import com.cowaine.youngjujang.ch6.domain.controller.converter.HotelRoomNumberCo
 import com.cowaine.youngjujang.ch6.domain.controller.resolver.ClientInfoArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,5 +22,13 @@ public class WebServerConfig implements WebMvcConfigurer {
      @Override
      public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
           resolvers.add(new ClientInfoArgumentResolver());
+     }
+     
+     @Override // 기존 설정된 httpMessageConverter대신 사용자가 추가한거 사용하는거 (기존꺼도 사용 : extendMessageConverters()
+     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+          // accept header : application/json, application/xml 따라 자동으로 변경되도록
+//          WebMvcConfigurer.super.configureMessageConverters(converters);
+          converters.add(new MappingJackson2HttpMessageConverter()); // convert json message
+          converters.add(new MappingJackson2XmlHttpMessageConverter());// convert xml message
      }
 }
