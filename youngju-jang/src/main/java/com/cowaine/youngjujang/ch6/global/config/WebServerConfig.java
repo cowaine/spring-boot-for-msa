f4652fe33a53b13a0b9d97d02f02e191a3544191
Youngju-Jang <joj1043@nate.com>
2023-10-05 20:34:10 +0900

    [Feat] HandlerInterceptor (324p~)

--- youngju-jang/src/main/java/com/cowaine/youngjujang/ch6/global/config/WebServerConfig.java
+++ youngju-jang/src/main/java/com/cowaine/youngjujang/ch6/global/config/WebServerConfig.java
@@ -12,7 +12,9 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import java.util.List;
 
@@ -42,5 +44,15 @@ public class WebServerConfig implements WebMvcConfigurer {
           ObjectMapper objectMapper = new ObjectMapper();
           objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY); //<< 종류 많음 ㅇㅇ 317p 참고
           return objectMapper;
+     }
+     
+     @Override // 인터셉터 구현체 추가하기
+     public void addInterceptors(InterceptorRegistry registry) {
+//          WebMvcConfigurer.super.addInterceptors(registry);
+          LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+          localeChangeInterceptor.setParamName("locale"); // locale 파라미터값을 locale 객체로 변경
+          registry.addInterceptor(localeChangeInterceptor) // 인터셉터 추가.
+               .excludePathPatterns("/favicon.ico") // 인터셉터 기능 제외할 url패턴
+               .addPathPatterns("/**"); // 인터셉터가 동작할 경로 추가
      }
 }
