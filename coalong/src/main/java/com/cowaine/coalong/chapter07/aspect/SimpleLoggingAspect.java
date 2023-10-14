c5d805e3ba677b1a4681179e01c811a2ef1869c0
coalong <minay0904@gmail.com>
2023-10-14 21:40:58 +0900

    [chap7] Logging AOP 예제 추가 (발전)

--- coalong/src/main/java/com/cowaine/coalong/chapter07/aspect/SimpleLoggingAspect.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter07/aspect/SimpleLoggingAspect.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
@@ -17,14 +18,21 @@ public class SimpleLoggingAspect {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        String packageName = signature.getDeclaringType().getPackageName();
+        String classFullName = signature.getDeclaringTypeName();
+        String className = classFullName.replace(packageName + ".", "");
+        String methodName = signature.getMethod().getName();
+        String[] parameterNames = signature.getParameterNames();
+
         Object result;
         try {
             result = joinPoint.proceed();
         } finally {
             stopWatch.stop();
-            log.info("execution time: {} ms", stopWatch.getLastTaskTimeMillis());
+            log.info("{}#{}({}), execution time: {} ms", className, methodName, parameterNames,
+                    stopWatch.getLastTaskTimeMillis());
         }
-
         return result;
     }
 }
