496b613aa00380a74c2e35d7c355b59f4289072e
crupy <wlxh95@naver.com>
2023-10-21 17:08:23 +0900

    [Fix] 스프링 빈의 이름이 챕터 7과 중복되어 수정
    챕터 5의 HotelController와 챕터 7의 HotelController의 스프링 빈 이름이 중복되어서 다시 수정

--- crupy/src/main/java/com/cowaine/crupy/part5/controller/HotelController.java
+++ crupy/src/main/java/com/cowaine/crupy/part5/controller/HotelController.java
@@ -1,9 +1,11 @@
 package com.cowaine.crupy.part5.controller;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Component("part5HotelController")
 public class HotelController {
 
     @GetMapping("/hotels")
