c5d30ed6385871657eac0f4f812e9fb38faaa5e7
crupy <wlxh95@naver.com>
2023-10-27 23:25:48 +0900

    [Feat] Chapter 8 실습 코드
    Chapter 8.4까지 실습 코드

---
+++ crupy/src/main/java/com/cowaine/crupy/part8/domain/AbstractManageEntity.java
@@ -0,0 +1,31 @@
+package com.cowaine.crupy.part8.domain;
+
+import com.cowaine.crupy.part8.server.UserIdHolder;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.ZonedDateTime;
+
+@Slf4j
+@MappedSuperclass
+@Getter
+public class AbstractManageEntity {
+    @Column(name = "created_at")
+    private ZonedDateTime createdAt;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "modified_at")
+    private ZonedDateTime modifiedAt;
+
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    public AbstractManageEntity() {
+        this.createdAt = ZonedDateTime.now();
+        this.createdBy = UserIdHolder.getUserId();
+    }
+}
