45369779e34298c0dd708c19623167ba58c86db4
ahngliwoong <o_rea@naver.com>
2023-10-28 17:12:44 +0900

    [Chapter 08] JPA Practice

---
+++ ahngilwoong/src/main/java/com/cowaine/ahngilwoong/chapter8/domain/HotelRoomEntity.java
@@ -0,0 +1,45 @@
+package com.cowaine.ahngilwoong.chapter8.domain;
+
+
+import com.cowaine.ahngilwoong.chapter8.converter.HotelRoomTypeConverter;
+import com.cowaine.ahngilwoong.chapter8.enums.HotelRoomType;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@EqualsAndHashCode
+@Getter
+@ToString
+@Entity(name = "hotelRooms")
+@Table(name = "hotel_rooms")
+public class HotelRoomEntity extends AbstractManageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hotel_room_id")
+    private Long hotelRoomId;
+
+    @Column(name = "hotels_hotel_id")
+    private Long hotelId;
+
+    @Column(name = "room_number")
+    private String roomNumber;
+
+    @Column(name = "room_type")
+    @Convert(converter = HotelRoomTypeConverter.class)
+    private HotelRoomType roomType;
+
+    @Column(name = "original_price")
+    private BigDecimal originalPrice;
+
+    public static HotelRoomEntity of(String roomNumber, HotelRoomType hotelRoomType, BigDecimal originalPrice) {
+        HotelRoomEntity hotelRoomEntity = new HotelRoomEntity();
+        hotelRoomEntity.roomNumber = roomNumber;
+        hotelRoomEntity.roomType = hotelRoomType;
+        hotelRoomEntity.originalPrice = originalPrice;
+        return hotelRoomEntity;
+    }
+}
---
+++ ahngilwoong/src/main/java/com/cowaine/ahngilwoong/chapter8/service/HotelAuditListener.java
@@ -0,0 +1,23 @@
+package com.cowaine.ahngilwoong.chapter8.service;
+
+
+import com.cowaine.ahngilwoong.chapter8.domain.HotelEntity;
+import javax.persistence.PostPersist;
+import javax.persistence.PostRemove;
+import javax.persistence.PostUpdate;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HotelAuditListener {
+
+    @PostPersist
+    public void logWhenCreated(HotelEntity hotelEntity){
+        log.info("hotel created. id:{}", hotelEntity.getHotelId());
+    }
+
+    @PostUpdate
+    @PostRemove
+    public void logWhenChanged(HotelEntity hotelEntity) {
+        log.info("hotel changed. id:{}, name:{}", hotelEntity.getHotelId(), hotelEntity.getName());
+    }
+}
