4062af32bef7e25c54e79d7df5f049e55c3b94ec
coalong <minay0904@gmail.com>
2023-11-04 18:03:28 +0900

    [chap8] HotelRepository 생성

--- coalong/src/main/java/com/cowaine/coalong/chapter08/repository/HotelRepository.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter08/repository/HotelRepository.java
@@ -1,11 +1,19 @@
 package com.cowaine.coalong.chapter08.repository;
 
 import com.cowaine.coalong.chapter08.domain.HotelEntity;
+import com.cowaine.coalong.chapter08.domain.HotelStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
+//@Repository
 public interface HotelRepository extends JpaRepository<HotelEntity, Long> {
     Optional<HotelEntity> findById(Long id);
 
+    List<HotelEntity> findByStatus(HotelStatus hotelStatus);
+
 }
+
+
+
