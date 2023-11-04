6b42637b7773ccc3ba59f6f18ba481c6b651723c
cororing <corock.dev@gmail.com>
2023-11-04 17:23:29 +0900

    feat: Query 애너테이션을 사용한 쿼리 사용 예제 추가

--- corock/src/main/java/com/cowaine/corock/chapter08/repository/HotelRepository.java
+++ corock/src/main/java/com/cowaine/corock/chapter08/repository/HotelRepository.java
@@ -3,6 +3,8 @@ package com.cowaine.corock.chapter08.repository;
 import com.cowaine.corock.chapter08.domain.HotelEntity;
 import com.cowaine.corock.chapter08.domain.HotelStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.ZonedDateTime;
@@ -17,5 +19,11 @@ public interface HotelRepository extends JpaRepository<HotelEntity, Long> {
     // DELETE FROM hotels WHERE status = :status AND created_at BETWEEN :beginCreatedAt and :endCreatedAt AND name is NULL
     void deleteByStatusInAndCreatedDateBetweenAndNameIsNull(List<HotelStatus> hotelStatus, ZonedDateTime beginCreatedAt,
                                                             ZonedDateTime endCreatedAt);
+
+    @Query("SELECT h FROM hotels AS h WHERE h.hotelId = ?1 AND h.status = 0")
+    HotelEntity findReadyOne(Long hotelId);
+
+    @Query("SELECT h FROM hotels AS h WHERE h.hotelId = :hotelId AND h.status = :status")
+    HotelEntity findOne(@Param("hotelId") Long hotelId, @Param("status") HotelStatus hotelStatus);
 
 }
