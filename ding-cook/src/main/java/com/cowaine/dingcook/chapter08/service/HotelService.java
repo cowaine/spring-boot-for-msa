d91c39c8c360e4ca5d08591beb0b0c61ecf8977b
bunsung92 <bunsung92@naver.com>
2023-11-11 17:55:22 +0900

    feat: 챕터 8.8 ~. 8.11 코드 소급적용

--- ding-cook/src/main/java/com/cowaine/dingcook/chapter08/service/HotelService.java
+++ ding-cook/src/main/java/com/cowaine/dingcook/chapter08/service/HotelService.java
@@ -1,10 +1,10 @@
 package com.cowaine.dingcook.chapter08.service;
 
-import com.cowaine.dingcook.chapter07.domain.HotelRoomEntity;
 import com.cowaine.dingcook.chapter08.controller.HotelCreateRequest;
 import com.cowaine.dingcook.chapter08.controller.HotelCreateResponse;
 import com.cowaine.dingcook.chapter08.controller.HotelResponse;
 import com.cowaine.dingcook.chapter08.domain.HotelEntity;
+import com.cowaine.dingcook.chapter08.domain.HotelRoomEntity;
 import com.cowaine.dingcook.chapter08.domain.HotelRoomType;
 import com.cowaine.dingcook.chapter08.repository.HotelRepository;
 import java.math.BigDecimal;
