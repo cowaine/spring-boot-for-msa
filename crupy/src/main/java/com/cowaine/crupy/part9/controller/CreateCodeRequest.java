483586bee8a6da7439e8c34231073fb5cc1f0a66
crupy <wlxh95@naver.com>
2023-11-18 01:23:10 +0900

    [Feat] chapter9 예제코드
    - chapter9 예제코드 전체
    - 어플리케이션을 실행하면 오류가 나지만 아직 원인을 찾지 못했다.

---
+++ crupy/src/main/java/com/cowaine/crupy/part9/controller/CreateCodeRequest.java
@@ -0,0 +1,26 @@
+package com.cowaine.crupy.part9.controller;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class CreateCodeRequest {
+    private Integer type;
+
+    @JsonProperty("hotelIds")
+    private List<Long> ids;
+
+    public CreateCodeRequest() {
+    }
+
+    public CreateCodeRequest(Integer type, List<Long> ids) {
+        this.type = type;
+        this.ids = ids;
+    }
+}
