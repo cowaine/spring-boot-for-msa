7c60347036d0b7bd0da250541f16a4e68ec22eb3
coalong <minay0904@gmail.com>
2023-11-18 16:42:09 +0900

    [chap9] RestTemplate 실습 예제

---
+++ coalong/src/main/java/com/cowaine/coalong/chapter09/controller/BillingCodeResponse.java
@@ -0,0 +1,20 @@
+package com.cowaine.coalong.chapter09.controller;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class BillingCodeResponse {
+
+    private String billingCode;
+
+    public static BillingCodeResponse of(String billingCode) {
+        BillingCodeResponse response = new BillingCodeResponse();
+        response.billingCode = billingCode;
+        return response;
+    }
+}
