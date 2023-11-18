d55f8045372323f1b26093ba2540704383b5989a
corock <hoongineer@gmail.com>
2023-11-18 16:29:52 +0900

    refactor: afterCommit 메서드를 구현하여 트랜잭션과 REST-API 분리

--- corock/src/main/java/com/cowaine/corock/chapter09/hotel/HotelService09.java
+++ corock/src/main/java/com/cowaine/corock/chapter09/hotel/HotelService09.java
@@ -1,4 +1,4 @@
-package com.cowaine.corock.chapter08.service;
+package com.cowaine.corock.chapter09.hotel;
 
 import com.cowaine.corock.chapter08.controller.HotelCreateRequest;
 import com.cowaine.corock.chapter08.controller.HotelCreateResponse;
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -18,14 +20,15 @@ import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
-public class HotelService {
+public class HotelService09 {
 
     private final HotelRepository hotelRepository;
+    // private final BillingApiAdaptor billingApiAdaptor;
 
     @Transactional(readOnly = false, isolation = Isolation.SERIALIZABLE)
     public HotelCreateResponse createHotel(HotelCreateRequest createRequest) {
         HotelEntity hotelEntity = HotelEntity.of(createRequest.getName(), createRequest.getAddress(),
-                                                 createRequest.getPhoneNumber());
+                createRequest.getPhoneNumber());
 
         int roomCount = createRequest.getRoomCount();
         List<HotelRoomEntity> hotelRoomEntities = IntStream.range(0, roomCount)
@@ -34,6 +37,13 @@ public class HotelService {
         hotelEntity.addHotelRooms(hotelRoomEntities);
 
         HotelEntity savedHotelEntity = hotelRepository.save(hotelEntity);
+
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+            @Override
+            public void afterCommit() {
+                // billingApiAdaptor.registerHotelCode(hotelEntity.getHotelId());
+            }
+        });
 
         return HotelCreateResponse.of(hotelEntity.getHotelId());
     }
