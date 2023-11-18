8ec5d78c3577f805c9e7b87753c290f688fbf495
bunsung92 <bunsung92@naver.com>
2023-11-18 17:59:30 +0900

    feat: Chapter09Application 클래스 코드까지 추가 로그 이력 해결 필요 및 Application02, Application03 코드 추가 필요

--- ding-cook/src/main/java/com/cowaine/dingcook/chapter09/Chapter09Application.java
+++ ding-cook/src/main/java/com/cowaine/dingcook/chapter09/Chapter09Application.java
@@ -1,41 +1,30 @@
-package com.cowaine.dingcook;
+package com.cowaine.dingcook.chapter09;
 
+import com.cowaine.dingcook.DingCookApplication;
 import com.cowaine.dingcook.chapter09.adpater.BillingAdapter;
 import com.cowaine.dingcook.chapter09.controller.BillingCodeResponse;
 import com.cowaine.dingcook.chapter09.controller.CreateCodeResponse;
-import java.util.Arrays;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
 
-//@Slf4j
-//@SpringBootApplication
-public class DingCookApplication {
+@Slf4j
+@SpringBootApplication
+public class Chapter09Application {
 
     public static void main(String[] args) {
-//        ConfigurableApplicationContext context = SpringApplication.run(DingCookApplication.class, args);
-//        Environment environment = context.getBean(Environment.class);
-//        String portValue = environment.getProperty("server.port");
-//        log.info("Customized Port:{}", portValue);
-//        Arrays.stream(context.getBeanDefinitionNames())
-//              .forEach(name -> log.info("Bean Name: {}", name));
-
-        // Chapter09Application 코드 참조
         ConfigurableApplicationContext ctxt = SpringApplication.run(DingCookApplication.class, args);
-
         BillingAdapter billingAdapter = ctxt.getBean(BillingAdapter.class);
 
         List<BillingCodeResponse> responses = billingAdapter.getBillingCodes("CODE:1231231");
-//        log.info("1. Result : {}", responses);
+        log.info("1. Result : {}", responses);
 
         CreateCodeResponse createCodeResponse = billingAdapter.create(List.of(1231231L));
-//        log.info("2. Result : {}", createCodeResponse);
+        log.info("2. Result : {}", createCodeResponse);
 
         CreateCodeResponse codeResponse = billingAdapter.createBillingCode(List.of(9000L, 8000L, 7000L));
-//        log.info("3. Result : {}", codeResponse);
+        log.info("3. Result : {}", codeResponse);
     }
-
 }
---
+++ ding-cook/src/main/java/com/cowaine/dingcook/chapter09/controller/CreateCodeRequest.java
@@ -0,0 +1,25 @@
+package com.cowaine.dingcook.chapter09.controller;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CreateCodeRequest {
+    private Integer type;
+
+    @JsonProperty("hotelIds")
+    private List<Long> ids;
+
+    public CreateCodeRequest() {
+    }
+
+    public CreateCodeRequest(Integer type, List<Long> ids) {
+        this.type = type;
+        this.ids = ids;
+    }
+}
