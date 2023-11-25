64814d463b0eaeb5a49483310cd5b8b3c6461d09
cororing <corock.dev@gmail.com>
2023-11-25 16:40:21 +0900

    refactor: RedisSentinelConfiguration 을 사용한 레디스 센티넬 구성 설정

--- corock/src/main/java/com/cowaine/corock/chapter10/config/CacheConfig.java
+++ corock/src/main/java/com/cowaine/corock/chapter10/config/CacheConfig.java
@@ -5,11 +5,14 @@ import io.lettuce.core.SocketOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 import java.time.Duration;
+import java.util.List;
 
 @Configuration
 public class CacheConfig {
@@ -31,6 +34,17 @@ public class CacheConfig {
                 .build();
 
         return new LettuceConnectionFactory(configuration, lettuceClientConfiguration);
+    }
+
+    @Bean
+    public RedisConnectionFactory cacheRedisSentinelConnectionFactory() {
+        RedisSentinelConfiguration configuration = new RedisSentinelConfiguration();
+        configuration.setMaster("REDIS_MASTER_NAME");
+        List<RedisNode> redisNodes = List.of(new RedisNode("127.0.0.1", 19999),
+                new RedisNode("127.0.0.1", 19998), new RedisNode("127.0.0.1", 19997));
+        configuration.setSentinels(redisNodes);
+
+        return new LettuceConnectionFactory(configuration);
     }
 
 }
