f1fb55f1279c0e8c6e793888fde98d3844626cc1
coalong <minay0904@gmail.com>
2023-11-26 01:56:11 +0900

    [chap 10] 10.3.1 RedisStandaloneConfiguration 을 사용한 예제

---
+++ coalong/src/main/java/com/cowaine/coalong/chapter10/config/CacheConfig.java
@@ -0,0 +1,41 @@
+package com.cowaine.coalong.chapter10.config;
+
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+import java.time.Duration;
+
+@Slf4j
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public RedisConnectionFactory cacheRedisConnectionFactory() {
+        // 레디스 서버의 주소와 IP 설정
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration("127.0.0.1", 6379);
+        // 레디스의 데이터베이스 번호 설정 (0~15까지 구분하여 운영)
+        configuration.setDatabase(0);
+        configuration.setUsername("username");
+        configuration.setPassword("password");
+
+        // 레디스와 클라이언트 커넥션 생성할 때 최대 소요시간 설정
+        final SocketOptions socketOptions = SocketOptions.builder().connectTimeout(Duration.ofSeconds(10)).build();
+        final ClientOptions clientOptions = ClientOptions.builder().socketOptions(socketOptions).build();
+
+        LettuceClientConfiguration lettuceClientConfiguration = LettuceClientConfiguration.builder()
+                .clientOptions(clientOptions)
+                .commandTimeout(Duration.ofSeconds(5))  // 레디스 명령어를 실행하고 응답받는 시간을 설정
+                .shutdownTimeout(Duration.ZERO)         // 애플리케이션이 종료될 때까지 기다리는 최대 시간
+                .build();
+
+        return new LettuceConnectionFactory(configuration, lettuceClientConfiguration);
+    }
+
+}
