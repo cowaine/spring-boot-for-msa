e0cf62fae55e067dc443d8b94cba182efaac08ff
JoisFe <rhsnqk@gmail.com>
2023-11-25 16:26:45 +0900

    RedisAutoConfiguration 방식 -> RedisTemplate 빈을 직접 설정하는 방식을 사용할 것이므로 주석처리

--- joisfe/src/main/java/com/cowaine/joisfe/part10/adapter/RedisAdapter.java
+++ joisfe/src/main/java/com/cowaine/joisfe/part10/adapter/RedisAdapter.java
@@ -2,6 +2,7 @@ package com.cowaine.joisfe.part10.adapter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,4 +10,7 @@ public class RedisAdapter {
 
     @Autowired
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 }
