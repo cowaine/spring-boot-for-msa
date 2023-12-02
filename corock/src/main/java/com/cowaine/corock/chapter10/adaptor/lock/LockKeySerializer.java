204451677b1299ee393f9a02547e4002df7ecf4c
cororing <corock.dev@gmail.com>
2023-12-02 17:24:23 +0900

    feat: 레디스 분산 락 사용 예제에서 필요한 스프링 빈 추가 및 단위 테스트 작성

---
+++ corock/src/main/java/com/cowaine/corock/chapter10/adaptor/lock/LockKeySerializer.java
@@ -0,0 +1,26 @@
+package com.cowaine.corock.chapter10.adaptor.lock;
+
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
+
+import java.util.Objects;
+
+public class LockKeySerializer implements RedisSerializer<LockKey> {
+
+    @Override
+    public byte[] serialize(LockKey lockKey) throws SerializationException {
+        if (Objects.isNull(lockKey)) {
+            throw new SerializationException("lockKey is null");
+        }
+        return lockKey.toString().getBytes();
+    }
+
+    @Override
+    public LockKey deserialize(byte[] bytes) throws SerializationException {
+        if (Objects.isNull(bytes)) {
+            throw new SerializationException("bytes is null");
+        }
+        return LockKey.fromString(new String(bytes));
+    }
+
+}
