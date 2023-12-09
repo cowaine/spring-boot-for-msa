b7d8e72398c14cbc084c062351cc6d92ca1674e1
bunsung92 <bunsung92@naver.com>
2023-12-09 16:29:39 +0900

    feat: Chapter11 Config 설정 및 Test 코드 작성 완료

---
+++ ding-cook/src/main/java/com/cowaine/dingcook/chatper11/schedule/ScheduledTask.java
@@ -0,0 +1,28 @@
+package com.cowaine.dingcook.chatper11.schedule;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ScheduledTask {
+
+    private AtomicInteger atomicInteger = new AtomicInteger();
+
+    // @Scheduled(fixRate = 1000L)
+    // @Scheduled(cron = "0/10 * * * * ?)
+    @Scheduled(fixedRate = 1000)
+    public void triggerEvent() {
+        int i = atomicInteger.addAndGet(1);
+        log.info("Triggered Event : #{}", i);
+
+        try {
+            TimeUnit.SECONDS.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
