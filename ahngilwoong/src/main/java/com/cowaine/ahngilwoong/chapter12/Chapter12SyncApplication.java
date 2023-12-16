0495ae908ad7c10e5e1d46ed261703cb6e421ead
JoisFe <rhsnqk@gmail.com>
2023-12-16 17:29:40 +0900

    Revert "Revert "[Chapter 12]""

---
+++ ahngilwoong/src/main/java/com/cowaine/ahngilwoong/chapter12/Chapter12SyncApplication.java
@@ -0,0 +1,23 @@
+package com.cowaine.ahngilwoong.chapter12;
+
+import com.cowaine.ahngilwoong.chapter12.server.ApplicationEventListener;
+import com.cowaine.ahngilwoong.chapter12.service.UserService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Chapter12SyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplicationBuilder appBuilder = new SpringApplicationBuilder(Chapter12SyncApplication.class);
+        SpringApplication application = appBuilder.build();
+        application.addListeners(new ApplicationEventListener());
+
+        ConfigurableApplicationContext ctxt = application.run(args);
+
+        UserService userService = ctxt.getBean(UserService.class);
+        userService.createUser("Byungboo Kim", "test.com");
+    }
+}
