ef1fbad9ef933e6a891498e48056ab6b8ddf2cfd
cororing <corock.dev@gmail.com>
2023-12-16 16:17:31 +0900

    feat: EventListener 애너테이션 예제 추가

--- corock/src/main/java/com/cowaine/corock/chapter12/event/hotel/HotelCreateEvent.java
+++ corock/src/main/java/com/cowaine/corock/chapter12/event/hotel/HotelCreateEvent.java
@@ -1,4 +1,20 @@
 package com.cowaine.corock.chapter12.event.hotel;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@ToString
 public class HotelCreateEvent {
+
+    private final Long hotelId;
+    private final String hotelAddress;
+
+    public static HotelCreateEvent of(Long hotelId, String hotelAddress) {
+        return new HotelCreateEvent(hotelId, hotelAddress);
+    }
+
 }
