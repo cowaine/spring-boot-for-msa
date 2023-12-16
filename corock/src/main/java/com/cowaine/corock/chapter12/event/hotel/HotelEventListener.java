b09bab0ae88479f48856f62db977aace065cb073
cororing <corock.dev@gmail.com>
2023-12-16 16:28:18 +0900

    feat: 이벤트 전파 예제 추가

--- corock/src/main/java/com/cowaine/corock/chapter12/event/hotel/HotelEventListener.java
+++ corock/src/main/java/com/cowaine/corock/chapter12/event/hotel/HotelEventListener.java
@@ -1,5 +1,7 @@
 package com.cowaine.corock.chapter12.event.hotel;
 
+import com.cowaine.corock.chapter12.service.PropagationService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.Async;
@@ -8,13 +10,17 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class HotelEventListener {
+
+    private final PropagationService propagationService;
 
     @Async
     @Order(1)
     @TransactionalEventListener(classes = HotelCreateEvent.class, fallbackExecution = true)
     public void handleHotelCreateEvent(HotelCreateEvent hotelCreateEvent) {
         log.info("handle HotelCreatedEvent: {}", hotelCreateEvent);
+        propagationService.propagateHotelEvent();
     }
 
     @Async
@@ -22,6 +28,7 @@ public class HotelEventListener {
     @TransactionalEventListener(classes = HotelCreateEvent.class)
     public void handleResourceCreateEvent(HotelCreateEvent hotelCreateEvent) {
         log.info("handle resourceCreatedEvent: {}", hotelCreateEvent);
+        propagationService.propagateResourceEvent();
     }
 
 }
