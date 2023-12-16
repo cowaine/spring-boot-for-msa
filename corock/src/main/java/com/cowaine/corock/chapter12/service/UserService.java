5ae033eac925fee8da236470296805664c5d065b
cororing <corock.dev@gmail.com>
2023-12-16 15:40:53 +0900

    feat: 스프링 이벤트 장점 섹션 예제 추가

---
+++ corock/src/main/java/com/cowaine/corock/chapter12/service/UserService.java
@@ -0,0 +1,20 @@
+package com.cowaine.corock.chapter12.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final EventService eventService;
+
+    public Boolean createUser(String username, String emailAddress) {
+        // 사용자 생성 로직 생략
+        // (...)
+
+        eventService.sendEventMail(emailAddress);
+        return Boolean.TRUE;
+    }
+
+}
